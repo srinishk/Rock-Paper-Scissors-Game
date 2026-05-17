ea445027160ca95a002fbd1c5b733f9894545c7d
srinishk <srinishk.muddasani@gmail.com>
2026-05-17 16:31:21 +0530

    Main Project

--- Rock, Paper, Scissors Python TKinter.py
+++ Rock, Paper, Scissors Python TKinter.py
@@ -6,62 +6,78 @@ win.title("Rock, Paper, Scissors")
 win.geometry("400x300")
 
 
-global comp
-num = randint(1,3)
-
-if num == 1:
-  comp = "ROCK"
-elif num == 2:
-  comp = "PAPER"
-else:
-  comp = "SCISSORS"
-
 def compare1(pc):
-  if pc == comp:
-    lbl.config(text = f"Player choice is {pc} \nComputer choice is {comp}.\n Result is a Tie!")
 
-  elif pc == "ROCK" and comp == "PAPER":
-    lbl.config(text = f"Player choice is {pc} \nComputer choice is {comp}.\n You have lost.")
+    # Generate computer choice every round
+    num = randint(1, 3)
 
-  elif pc == "ROCK" and comp == "SCISSORS":
-    lbl.config(text = f"Player choice is {pc} \nComputer choice is {comp}.\n You won!")
+    if num == 1:
+        comp = "ROCK"
+    elif num == 2:
+        comp = "PAPER"
+    else:
+        comp = "SCISSORS"
 
-  elif pc == "PAPER" and comp == "ROCK":
-    lbl.config(text = f"Player choice is {pc} \nComputer choice is {comp}.\n You won!")
+    if pc == comp:
+        lbl.config(
+            text=f"Player choice is {pc}\nComputer choice is {comp}.\nResult is a Tie!"
+        )
 
-  elif pc == "PAPER" and comp == "SCISSORS":
-    lbl.config(text = f"Player choice is {pc} \nComputer choice is {comp}.\n You have lost.")
+    elif pc == "ROCK" and comp == "PAPER":
+        lbl.config(
+            text=f"Player choice is {pc}\nComputer choice is {comp}.\nYou have lost."
+        )
 
-  elif pc == "SCISSORS" and comp == "ROCK":
-    lbl.config(text = f"Player choice is {pc} \nComputer choice is {comp}.\n You have lost.")
+    elif pc == "ROCK" and comp == "SCISSORS":
+        lbl.config(
+            text=f"Player choice is {pc}\nComputer choice is {comp}.\nYou won!"
+        )
 
-  elif pc == "SCISSORS" and comp == "PAPER":
-    lbl.config(text = f"Player choice is {pc} \nComputer choice is {comp}.\n You won!")
-  
-  else:
-    lbl.config(text = "Error")
+    elif pc == "PAPER" and comp == "ROCK":
+        lbl.config(
+            text=f"Player choice is {pc}\nComputer choice is {comp}.\nYou won!"
+        )
 
+    elif pc == "PAPER" and comp == "SCISSORS":
+        lbl.config(
+            text=f"Player choice is {pc}\nComputer choice is {comp}.\nYou have lost."
+        )
+
+    elif pc == "SCISSORS" and comp == "ROCK":
+        lbl.config(
+            text=f"Player choice is {pc}\nComputer choice is {comp}.\nYou have lost."
+        )
+
+    elif pc == "SCISSORS" and comp == "PAPER":
+        lbl.config(
+            text=f"Player choice is {pc}\nComputer choice is {comp}.\nYou won!"
+        )
+
+    else:
+        lbl.config(text="Error")
 
 
 def rock():
-  compare1("ROCK")
+    compare1("ROCK")
+
 
 def paper():
-  compare1("PAPER")
+    compare1("PAPER")
+
 
 def scissors():
-  compare1("SCISSORS")
-  
+    compare1("SCISSORS")
 
-br = Button(win, text = "ROCK", width = 30, height = 3, command = rock)
-bp = Button(win, text = "PAPER", width = 30, height = 3, command = paper)
-bs = Button(win, text = "SCISSORS", width = 30, height = 3, command = scissors)
 
-lbl = Label(win, text = comp, width = 30, height = 3)
+br = Button(win, text="ROCK", width=30, height=3, command=rock)
+bp = Button(win, text="PAPER", width=30, height=3, command=paper)
+bs = Button(win, text="SCISSORS", width=30, height=3, command=scissors)
 
-br.pack(pady = 5)
-bp.pack(pady = 5)
-bs.pack(pady = 5)
+lbl = Label(win, text="Choose an option", width=40, height=5)
+
+br.pack(pady=5)
+bp.pack(pady=5)
+bs.pack(pady=5)
 lbl.pack()
 
 win.mainloop()
